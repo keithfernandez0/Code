5a4ba40c7d6023214c931906f61183b0d0e6c512
Keith Fernandez <keithfernandez000@gmail.com>
2022-11-30 15:13:25 -0500

    Changes to be committed: 	modified:   Java/RadiationCalculator.java

--- Java/RadiationCalculator.java
+++ Java/RadiationCalculator.java
@@ -43,35 +43,50 @@ public class RadiationCalculator {
                 System.out.println("            [1] ~ Uranium-235");
                 System.out.println("            [2] ~ Americium-241");
                 System.out.println("            [3] ~ Cobalt-60");
-                System.out.println("            [4] ~ Plutonium-241\n");
+                System.out.println("            [4] ~ Plutonium-238\n");
                 System.out.println("===================================================\n");
 
                 choice1 = input.nextInt();
                 switch (choice1) {
                     case 1:
                         System.out.println("\nU-235 is an isotope of Uranium making up 0.72% of naturally occurring uranium.\n"
-                                         + "U-235 is a fissile material, meaning it can sustain a self-sustaining energetic\n"
-                                         + "reaction. U-235 is known for its uses in both nuclear power plants to generate\n"
-                                         + "electricity to the power grid, but as well as its application in bombs.");
+                                         + " U-235 is a fissile material, meaning it can sustain a self-sustaining energetic\n"
+                                         + " reaction. U-235 is known for its uses in both nuclear power plants to generate\n"
+                                         + " electricity to the power grid, but as well as its application in bombs.");
                         break;
 
                     case 2:
                         System.out.println("\nAm-241 is an isotope of americium, with a half-life of 432.2 years.\n"
-                                         + "It is commonly found in ionization-type smoke detectors for households.\n"
-                                         + "Am-241 is also used in radioisotope thermoelectric generators (RTGs)\n"
-                                         + "which are used in spacecraft. Additionally, this isotope has application\n"
-                                         + "in medicine to help diagnose issues with the thyroid.");
+                                         + " It is commonly found in ionization-type smoke detectors for households.\n"
+                                         + " Am-241 is also used in radioisotope thermoelectric generators (RTGs)\n"
+                                         + " which are used in spacecraft. Additionally, this isotope has application\n"
+                                         + " in medicine to help diagnose issues with the thyroid.");
                         break;
 
                     case 3:
                         System.out.println("\nCobalt-60 is a synthetic (manmade) radioactive isotope, with a\n"
-                                         + "half-life of 5.3 years. It is produced synthetically in nuclear reactors\n"
-                                         + "and is a high-intensity isotope when compared to other radioactive elements.\n"
-                                         + "Its applications are in the sterilization of medical equipment, and industrial\n"
-                                         + "testing of materials. In radiotherapy, a technician can use a machine built\n"
-                                         + "for Co-60 and treat cancers without invasive surgery."); 
-                        break;             
+                                         + " half-life of 5.3 years. It is produced synthetically in nuclear reactors\n"
+                                         + " and is a high-intensity isotope when compared to other radioactive elements.\n"
+                                         + " Its applications are in the sterilization of medical equipment, and industrial\n"
+                                         + " testing of materials. In radiotherapy, a technician can use a machine built\n"
+                                         + " for Co-60 and treat cancers without invasive surgery."); 
+                        break;     
+
                     case 4:
+                        System.out.println("\nPu-238 is an isotope with a half-life of 87.7 years, and is also used as\n"
+                                         + " spacecraft energy generators. Plutonium was first synthesized in 1940-1941\n"
+                                         + " and its use in the now extremely unethical *Plutonium Injection Experiments*\n"
+                                         + " carried out by researchers and physicians at the Los Alamos National Lab, from\n"
+                                         + " 1945-1947. Plutonium-238 was critical in the development of US-sponsored Manhatten\n"
+                                         + " Project on the cusp of World War II. It is also notably used in the energy systems"
+                                         + " of the Pioneer 10 and 11, Voyager 1 and 2, and the 2020 Mars Perserverance Rover.\n\n"
+
+                                         + "For more information regarding the controversial human radiation experiments, a free\n"
+                                         + " archival PDF is provided in the following link:\n\n"
+                                         + "https://ia803202.us.archive.org/16/items/1999-ew-the-plutonium-files/1999%20EW%20The%20Plutonium%20Files.pdf\n");
+
+                        break;
+                        
                     default:
                         System.out.println("Not a valid input!\nSystem exit status [1].");
                         System.exit(1);
