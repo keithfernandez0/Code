3b7717cc8315deaad3162ddf261ffcb855670400
Keith Fernandez <keithfernandez000@gmail.com>
2022-12-05 10:18:43 -0500

    Changes to be committed: 	modified:   Java/RadiationCalculator.java

--- Java/RadiationCalculator.java
+++ Java/RadiationCalculator.java
@@ -38,13 +38,14 @@ public class RadiationCalculator {
                         "║║║╚╣╔╗║╚╝║║╔╗║╚╣║╚╝║║║║║╚═╝║╔╗║╚╣╚═╣╚╝║╚╣╔╗║╚╣╚╝║║\n" +
                         "╚╝╚═╩╝╚╩══╩╩╝╚╩═╩╩══╩╝╚╝╚═══╩╝╚╩═╩══╩══╩═╩╝╚╩═╩══╩╝\n\n";
         
-        System.out.println("CPS 1231 Radiation Education + Simple Calculator Project");
+        System.out.println("Keith Fernandez, Jason Zaruma, Ayman Rashiel, James Hernandez");                
+        System.out.println("CPS 1231 Radiation Education + Simple EPA Calculator Project");
         System.out.print(logo);
         System.out.println("===================================================\n");
         System.out.println("            Select an option from 1-3.\n");
         System.out.println("            [1] ~ Isotope information");
-        System.out.println("            [2] ~ EPA Radiation calculator\n");
-        System.out.println("            [3] ~ Links to media/data\n");
+        System.out.println("            [2] ~ EPA Radiation calculator");
+        System.out.println("            [3] ~ Links to media + data\n");
         System.out.println("===================================================\n");
 
         choice = input.nextInt();
@@ -65,45 +66,45 @@ public class RadiationCalculator {
                 switch (choice1) {
                     case 1:
                         System.out.println("\nU-235 is an isotope of Uranium making up 0.72% of naturally occurring uranium.\n"
-                                         + " U-235 is a fissile material, meaning it can sustain a self-sustaining energetic\n"
-                                         + " reaction. U-235 is known for its uses in both nuclear power plants to generate\n"
-                                         + " electricity to the power grid, but as well as its application in bombs.");
+                                         + "U-235 is a fissile material, meaning it can sustain a self-sustaining energetic\n"
+                                         + "reaction. U-235 is known for its uses in both nuclear power plants to generate\n"
+                                         + "electricity to the power grid, but as well as its application in bombs.\n");
                         break;
 
                     case 2:
                         System.out.println("\nAm-241 is an isotope of americium, with a half-life of 432.2 years.\n"
-                                         + " It is commonly found in ionization-type smoke detectors for households.\n"
-                                         + " Am-241 is also used in radioisotope thermoelectric generators (RTGs)\n"
-                                         + " which are used in spacecraft. Additionally, this isotope has application\n"
-                                         + " in medicine to help diagnose issues with the thyroid.");
+                                         + "It is commonly found in ionization-type smoke detectors for households.\n"
+                                         + "Am-241 is also used in radioisotope thermoelectric generators (RTGs)\n"
+                                         + "which are used in spacecraft. Additionally, this isotope has application\n"
+                                         + "in medicine to help diagnose issues with the thyroid.\n");
                         break;
 
                     case 3:
                         System.out.println("\nCobalt-60 is a synthetic (manmade) radioactive isotope, with a\n"
-                                         + " half-life of 5.3 years. It is produced synthetically in nuclear reactors\n"
-                                         + " and is a high-intensity isotope when compared to other radioactive elements.\n"
-                                         + " Its applications are in the sterilization of medical equipment, and industrial\n"
-                                         + " testing of materials. In radiotherapy, a technician can use a machine built\n"
-                                         + " for Co-60 and treat cancers without invasive surgery."); 
+                                         + "half-life of 5.3 years. It is produced synthetically in nuclear reactors\n"
+                                         + "and is a high-intensity isotope when compared to other radioactive elements.\n"
+                                         + "Its applications are in the sterilization of medical equipment, and industrial\n"
+                                         + "testing of materials. In radiotherapy, a technician can use a machine built\n"
+                                         + "for Co-60 and treat cancers without invasive surgery.\n"); 
                         break;     
 
                     case 4:
                         System.out.println("\nPu-238 is an isotope with a half-life of 87.7 years, and is also used as\n"
-                                         + " spacecraft energy generators. Plutonium was first synthesized in 1940-1941\n"
-                                         + " and its use in the now extremely unethical *Plutonium Injection Experiments*\n"
-                                         + " carried out by researchers and physicians at the Los Alamos National Lab, from\n"
-                                         + " 1945-1947. Plutonium-238 was critical in the development of US-sponsored Manhatten\n"
-                                         + " Project on the cusp of World War II. It is also notably used in the energy systems"
-                                         + " of the Pioneer 10 and 11, Voyager 1 and 2, and the 2020 Mars Perserverance Rover.\n\n"
+                                         + "spacecraft energy generators. Plutonium was first synthesized in 1940-1941\n"
+                                         + "and its use in the now extremely unethical *Plutonium Injection Experiments*\n"
+                                         + "carried out by researchers and physicians at the Los Alamos National Lab, from\n"
+                                         + "1945-1947. Plutonium-238 was critical in the development of US-sponsored Manhatten\n"
+                                         + "Project on the cusp of World War II. It is also notably used in the energy systems\n"
+                                         + "of the Pioneer 10 and 11, Voyager 1 and 2, and the 2020 Mars Perserverance Rover.\n\n"
 
                                          + "For more information regarding the controversial human radiation experiments, a free\n"
-                                         + " archival PDF is provided in the following link:\n\n"
+                                         + "archival PDF is provided in the following link:\n\n"
                                          + "https://ia803202.us.archive.org/16/items/1999-ew-the-plutonium-files/1999%20EW%20The%20Plutonium%20Files.pdf\n");
 
                         break;
 
                     default:
-                        System.out.println("Not a valid input!\nSystem exit status [1].");
+                        System.out.println("Not a valid input!\nSystem exit status [1].\n");
                         System.exit(1);
                 }
 
@@ -118,7 +119,7 @@ public class RadiationCalculator {
                                  + "  Kean University, so this may not be totally\n"
                                  + "  accurate. Answer to the best of your ability, \n"
                                  + "          using integer answers only.\n\n"
-                                 + " A final calculation and info about radiation will"
+                                 + " A final calculation and info about radiation will\n"
                                  + "            be given to you. Enjoy!");
                 System.out.println("=================================================\n");
 
@@ -137,25 +138,26 @@ public class RadiationCalculator {
                 finalSum = cigSum + xraySum + ctSum + envSum;
                 microSievert = finalSum * 10; // unit conversion
 
-                System.out.println("FINAL YEARLY ESTIMATED DOSE: " + finalSum + " mili-rem, and " + microSievert + " micro-Sieverts.\n");
+                System.out.println("\nFINAL YEARLY ESTIMATED DOSE: " + finalSum + " mili-rem, and " + microSievert + " micro-Sieverts.\n");
 
                 System.out.println("Reference:");
-                System.out.println("0.1 micro-Sievert = radiation from eating a banana.");
-                System.out.println("5 micro-Sieverts = dose from getting a dental x-ray.");
-                System.out.println("50 micro-Sieverts = dose from spending one hour on the ground at Chernobyl in 2010.");
-                System.out.println("100 micro-Sieverts = dose from getting a chest x-ray.");
-                System.out.println("400 micro-Sieverts = dose from getting a mammogram.");
-                System.out.println("1,000 micro-Sieverts = US government yearly limit on artificial radiation exposure to the public.");
-                System.out.println("10,000 micro-Sieverts = dose from an average CT scan");
-                System.out.println("50,000 micro-Sieverts = yearly maximum dose for US radiation workers");
-                System.out.println("250,000 micro-Sieverts = dose limit for US radiation workers in life-saving operations");
-                System.out.println("1,000,000 micro-Sieverts = non-fatal radiation sickness, nausea and low blood cell count");
-                System.out.println("2,000,000 micro-Sieverts = high energy targeted dose in radiotherapy");
-                System.out.println("4,000,000 micro-Sieverts = severe dose, bleeding / hair-loss, death within 4-6 weeks if untreated.");
-                System.out.println("6,000,000 micro-Sieverts = typically fatal in 2-4 weeks if untreated");
-                System.out.println("10,000,000 micro-Sieverts = fatal dose, death within 2 weeks.");
-                System.out.println("30,000,000 micro-Sieverts = seizures and tremors, death within 48 hours.");
-                System.out.println("50,000,000 micro-Sieverts = 10 minutes exposure to Chernobyl reactor core after meltdown.");
+                System.out.println("===================================================\n");
+                System.out.println("1.) 0.1 micro-Sievert = radiation from eating a banana.");
+                System.out.println("2.) 5 micro-Sieverts = dose from getting a dental x-ray.");
+                System.out.println("3.) 50 micro-Sieverts = dose from spending one hour on the ground at Chernobyl in 2010.");
+                System.out.println("4.) 100 micro-Sieverts = dose from getting a chest x-ray.");
+                System.out.println("5.) 400 micro-Sieverts = dose from getting a mammogram.");
+                System.out.println("6.) 1,000 micro-Sieverts = US government yearly limit on artificial radiation exposure to the public.");
+                System.out.println("7.) 10,000 micro-Sieverts = dose from an average CT scan");
+                System.out.println("8.) 50,000 micro-Sieverts = yearly maximum dose for US radiation workers");
+                System.out.println("9.) 250,000 micro-Sieverts = dose limit for US radiation workers in life-saving operations");
+                System.out.println("10.) 1,000,000 micro-Sieverts = non-fatal radiation sickness, nausea and low blood cell count");
+                System.out.println("11.) 2,000,000 micro-Sieverts = high energy targeted dose in radiotherapy");
+                System.out.println("12.) 4,000,000 micro-Sieverts = severe dose, bleeding / hair-loss, death within 4-6 weeks if untreated.");
+                System.out.println("13.) 6,000,000 micro-Sieverts = typically fatal in 2-4 weeks if untreated");
+                System.out.println("14.) 10,000,000 micro-Sieverts = fatal dose, death within 2 weeks.");
+                System.out.println("15.) 30,000,000 micro-Sieverts = seizures and tremors, death within 48 hours.");
+                System.out.println("16.) 50,000,000 micro-Sieverts = 10 minutes exposure to Chernobyl reactor core after meltdown.\n");
 
                 break;
             
@@ -165,11 +167,12 @@ public class RadiationCalculator {
                 System.out.println("https://ia803202.us.archive.org/16/items/1999-ew-the-plutonium-files/1999%20EW%20The%20Plutonium%20Files.pdf\n");
                 System.out.println("2. Radiation Dosage Chart, *InformationIsBeautiful.net*");
                 System.out.println("https://www.informationisbeautiful.net/visualizations/radiation-dosage-chart/\n");
+                
+                break;
 
             default:
-                System.out.println("Not a valid input!\nSystem exit status [1].");
+                System.out.println("Not a valid input!\nSystem exit status [1].\n");
                 System.exit(1);
-
 
         }
     }
