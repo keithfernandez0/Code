f303d31cbeb6c2fb67ec183e45ca6ea73c65f422
Keith Fernandez <keithfernandez000@gmail.com>
2023-02-12 18:37:14 -0500

    Changes to be committed: 	renamed:    Java/semester2/arrayActivity.java -> Java/semester2/arrays/arrayActivity.java 	renamed:    Java/semester2/arrayRand.java -> Java/semester2/arrays/arrayRand.java#     renamed:    Java/semester2/arrays.java -> Java/semester2/arrays/arrays.java 	renamed:    Java/semester2/discussionarray.java -> Java/semester2/arrays/discussionarray.java 	new file:   Java/semester2/arrays/twoDarray.java

--- Java/semester2/arrays/discussionarray.java
+++ Java/semester2/arrays/discussionarray.java
@@ -1,4 +1,4 @@
-package semester2;
+package semester2.arrays;
 import java.util.Scanner;
 
 public class discussionarray {
---
+++ Java/semester2/arrays/twoDarray.java
@@ -0,0 +1,32 @@
+package semester2.arrays;
+
+public class twoDarray {
+    public static void main(String[] args) {
+
+        // remember that we count from 0 instead of 1.
+
+        int[][] numArray = new int[2][2];       // 3x3
+        String[][] names = new String[3][4];    // 4x5
+        char[][] info = new char[8][6];         // 9x7
+        double[][] matrix = new double[3][3];   // 4x4
+
+        int[][] array = { // you may specify by element as well
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        System.out.println("Number of rows: " + numArray.length);
+        System.out.println("Number of columns: " + numArray[0].length);
+
+        // raggedy ass array
+
+        int[][] raggedMatrix = {
+            {1,2,3,4,5},
+            {2,3,4,5},
+            {3,4,5},
+            {4,5},
+            {5}
+        };
+    }
+}
