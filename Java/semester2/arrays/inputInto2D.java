a13f9b973059fc7d0dde082aa517f6cd691019b3
Keith Fernandez <keithfernandez000@gmail.com>
2023-02-12 18:36:24 -0500

    Changes to be committed: 	new file:   Java/semester2/arrays/inputInto2D.java
     Changes not staged for commit:
    	deleted:    Java/semester2/arrayActivity.java#
    	deleted:    Java/semester2/arrayRand.java
    	deleted:    Java/semester2/arrays.java
    	deleted:    Java/semester2/discussionarray.java

---
+++ Java/semester2/arrays/inputInto2D.java
@@ -0,0 +1,26 @@
+package semester2.arrays;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class inputInto2D {
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[3][3];
+        System.out.println("\nInitialized matrix:");
+        System.out.println(Arrays.deepToString(matrix) + "\n");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter " + matrix.length + " rows and " + matrix[0].length + " columns:\n");
+
+        for (int row=0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = input.nextInt();
+            }
+        }
+
+        System.out.println("\nFinalized matrix:");
+        System.out.println(Arrays.deepToString(matrix));
+
+    }
+    
+}
