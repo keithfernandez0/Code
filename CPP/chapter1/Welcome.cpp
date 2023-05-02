7471cf1646c1fde43626ab85b3ee126a5c2b4d07
Keith Fernandez <keithfernandez000@gmail.com>
2023-05-02 13:19:50 -0400

    Changes to be committed: 	new file:   .vscode/c_cpp_properties.json 	modified:   .vscode/launch.json 	modified:   .vscode/settings.json 	new file:   CPP/chapter1/Welcome.cpp 	new file:   CPP/chapter1/WelcomeWithThreeMessages.cpp

---
+++ CPP/chapter1/Welcome.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  // preprocessor directive + header file
+using namespace std;
+
+int main() {
+    
+    // Displays 'Welcome to C++!' to the console
+    cout << "Welcome to C++!" << endl;
+
+    return 0;
+}
---
+++ CPP/chapter1/WelcomeWithThreeMessages.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+cout << "Programming is fun!" << endl;
+cout << "Fundamentals first!" << endl;
+cout << "Problem driven!" << endl;
+
+return 0;
+}
