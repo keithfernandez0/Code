f83abf81146041d07d26997a22e9d6fb18e717dc
Keith Fernandez <keithfernandez000@gmail.com>
2023-05-05 21:13:47 -0400

    Changes to be committed: 	new file:   Java/year1/semester2/fernkayl_program.java  Changes not staged for commit: 	deleted:    Java/year1/semester2/HW7/fernkayl_program.java

---
+++ Java/year1/semester2/fernkayl_program.java
@@ -0,0 +1,37 @@
+/**************************************************************
+ * Kean University
+ * Spring 2023
+ * Course: CPS*2231 - Computer Programming in Java
+ * Author: Keith Michelangelo Fernandez
+ * 
+ * HW Assignment 7
+ **************************************************************
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class fernkayl_program {
+
+    public static void main(String[] args) throws Exception
+    {
+        String num, name, birthDate, price, special, specialFeature;
+
+        File file = new File("input.csv");
+
+        Scanner input = new Scanner(file);
+        input.useDelimiter(",");
+
+        while(input.hasNext())
+        {
+            name = input.next();
+            birthDate = input.next();
+            price = input.next();
+            special = input.next();
+            specialFeature = input.next();
+
+            System.out.println("Name: " + name + " BirthDate: " + birthDate + " Price: " + " Variety: " + special + " Special Feature: " + specialFeature);
+        }
+    }
+
+}
