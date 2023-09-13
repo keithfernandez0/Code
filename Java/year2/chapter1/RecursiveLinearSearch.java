fd3a7a1d93f6fcd8eeca5ab68e22183b91d15c45
Keith Fernandez <keithfernandez000@gmail.com>
2023-09-13 14:07:12 -0400

    Changes to be committed: 	modified:   .vscode/launch.json 	modified:   Java/year2/chapter1/LinearSearch.java 	new file:   Java/year2/chapter1/RecursiveLinearSearch.java

--- Java/year2/chapter1/RecursiveLinearSearch.java
+++ Java/year2/chapter1/RecursiveLinearSearch.java
@@ -8,32 +8,37 @@
 // 9/13/2023
 // ====================================================================================
 
+
 package Java.year2.chapter1;
 
-
-public class LinearSearch 
+public class RecursiveLinearSearch 
 {
-    public static int LinearSearch(int[] list, int key) 
+    public static int recursiveLinearSearch(int[] list, int key, int i) 
     {
-      for (int i = 0; i < list.length; i++) 
-      {
-        if (key == list[i])
-          return i;
-      }
-      return -1;
+        if (i >= list.length) 
+        {
+            return -1; 
+        }
+
+        if (list[i] == key) 
+        {
+            return i;
+        }
+
+        // FUNCTION WILL CALL BACK ONTO ITSELF
+        return recursiveLinearSearch(list, key, i + 1);
     }
-  
+
     public static void main(String[] args) 
     {
         final int N = 10000000;
         final int KEY = 150;
         int[] list = new int[N];
 
-        for (int i =
-         0; i < N; i++)
+        for (int i = 0; i < N; i++)
         {
             // GENERATOR FOR POSITIVE + NEGATIVE INTEGERS FROM -100 TO +100
-            list [i] = (int)(Math.random() * 100 * (Math.random() > 0.5 ? 1 : -1));
+            list[i] = (int)(Math.random() * 100 * (Math.random() > 0.5 ? 1 : -1));
             // STD OUT: PRINT THE NUMBERS
             System.out.println(list[i] + ",");
         }
@@ -44,12 +49,12 @@ public class LinearSearch
         // START STOPWATCH
         begin = System.nanoTime();
         System.out.println("\n>> [Starting nanoTime() timer...]");
-        System.out.println(">> [Program return status on LinearSearch(): " + LinearSearch(list, KEY) + "]");
+        System.out.println(">> [Program return status on RecursiveLinearSearch(): " + recursiveLinearSearch(list, KEY, 0) + "]");
         end = System.nanoTime();
         System.out.println(">> [Ended nanoTime() timer.]");
         System.out.println(">> [Calculating time in nanoseconds...]\n");
         time = end - begin;
-        System.out.println("It took " + time + " nanoseconds to run LinearSearch() with the key " + KEY + " on the array of " + N + " elements.");
+        System.out.println("It took " + time + " nanoseconds to run RecursiveLinearSearch() with the key " + KEY + " on the array of " + N + " elements.");
     }
-  }
-  
+}
+
