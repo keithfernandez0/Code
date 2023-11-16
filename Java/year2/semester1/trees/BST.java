e4f767c3db341d2dd01e54bdca54aff887ad42d2
Keith Fernandez <keithfernandez000@gmail.com>
2023-11-15 23:24:29 -0500

    Changes to be committed: 	modified:   .vscode/launch.json 	modified:   Java/year2/semester1/trees/BST.java 	deleted:    Java/year2/semester1/trees/BSTExample.java 	new file:   Java/year2/semester1/trees/BST_class.java

--- Java/year2/semester1/trees/BST.java
+++ Java/year2/semester1/trees/BST.java
@@ -1,131 +1,84 @@
 package Java.year2.semester1.trees;
 
-public class BST<E extends Comparable<E>> implements Tree<E> 
-{
-    protected TreeNode<E> root;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
-    public static class TreeNode<E> 
-    {
+public class BST<E extends Comparable<E>> implements Iterable<E> {
+
+    public static class TreeNode<E> {
         protected E element;
         protected TreeNode<E> left;
         protected TreeNode<E> right;
 
-        public TreeNode(E e) 
-        {
+        public TreeNode(E e) {
             element = e;
         }
     }
 
-    // SEARCH METHOD
-    public boolean search(E target) 
-    {
-        return searchHelper(root, target);
+    private TreeNode<E> root;
+    private int size;
+
+    public void insert(E e) {
+        // Implementation of the insert method
+        // ...
     }
 
-    private boolean searchHelper(TreeNode<E> node, E target) 
-    {
-        if (node == null) 
-        {
-            return false;
-        }
-
-        int compareResult = target.compareTo(node.element);
-
-        if (compareResult == 0) 
-        {
-            return true; // Found the target
-        } 
-        else if (compareResult < 0) 
-        {
-            return searchHelper(node.left, target); // Search in the left subtree
-        } 
-        else 
-        {
-            return searchHelper(node.right, target); // Search in the right subtree
+    public void addAll(List<E> elements) {
+        for (E element : elements) {
+            insert(element);
         }
     }
 
-    // INSERT METHOD
-    public void insert(E element) 
-    {
-        root = insertHelper(root, element);
+    @Override
+    public Iterator<E> iterator() {
+        return new BSTIterator();
     }
 
-    private TreeNode<E> insertHelper(TreeNode<E> node, E element) 
-    {
-        if (node == null) 
-        {
-            return new TreeNode<>(element);
+    private class BSTIterator implements Iterator<E> {
+        private TreeNode<E> current;
+        private java.util.Stack<TreeNode<E>> stack;
+    
+        public BSTIterator() {
+            stack = new java.util.Stack<>();
+            current = root;
+            initializeStack();
         }
-
-        int compareResult = element.compareTo(node.element);
-
-        if (compareResult < 0) 
-        {
-            node.left = insertHelper(node.left, element); // Insert in the left subtree
-        } 
-        else if (compareResult > 0) 
-        {
-            node.right = insertHelper(node.right, element); // Insert in the right subtree
-        }
-
-        return node;
-    }
-
-    // REMOVE METHOD
-    public void remove(E element) 
-    {
-        root = removeHelper(root, element);
-    }
-
-    private TreeNode<E> removeHelper(TreeNode<E> node, E element) 
-    {
-        if (node == null) 
-        {
-            return null;
-        }
-
-        int compareResult = element.compareTo(node.element);
-
-        if (compareResult < 0) 
-        {
-            node.left = removeHelper(node.left, element); // Remove in the left subtree
-        } 
-        else if (compareResult > 0) 
-        {
-            node.right = removeHelper(node.right, element); // Remove in the right subtree
-        } 
-        else 
-        {
-            // Node to be removed found
-
-            // Case 1: Node with only one child or no child
-            if (node.left == null) 
-            {
-                return node.right;
-            } 
-            else if (node.right == null) 
-            {
-                return node.left;
+    
+        private void initializeStack() {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
             }
-
-            // Case 2: Node with two children
-            node.element = findMin(node.right).element;
-            node.right = removeHelper(node.right, node.element);
         }
-
-        return node;
+    
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+    
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+    
+            TreeNode<E> temp = stack.pop();
+            current = temp.right;
+            initializeStack();
+    
+            return temp.element;
+        }
     }
 
-    private TreeNode<E> findMin(TreeNode<E> node) 
-    {
-        while (node.left != null) 
-        {
-            node = node.left;
-        }
-        return node;
-    }
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        bst.addAll(List.of(5, 2, 8, 1, 3, 7, 9));
 
-    // ADD OTHER METHODS (INORDER, ETC.) HERE
+        // Use for-each loop
+        for (Integer value : bst) {
+            System.out.print(value + " ");
+        }
+    }
 }
 
