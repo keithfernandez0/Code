d7a560817ccc0bafb3f0c693845fd833f10df5de
Keith Fernandez <keithfernandez000@gmail.com>
2023-11-27 02:50:36 -0500

    Changes to be committed: 	modified:   .vscode/launch.json 	modified:   Java/year2/semester1/trees/BST.java 	deleted:    Java/year2/semester1/trees/BST_class.java

--- Java/year2/semester1/trees/BST.java
+++ Java/year2/semester1/trees/BST.java
@@ -1,84 +1,128 @@
 package Java.year2.semester1.trees;
+import java.util.*;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
+class BST<E extends Comparable<E>> {
+    // node class that defines BST node
+    class Node {
+        E element;
+        Node left, right;
 
-public class BST<E extends Comparable<E>> implements Iterable<E> {
-
-    public static class TreeNode<E> {
-        protected E element;
-        protected TreeNode<E> left;
-        protected TreeNode<E> right;
-
-        public TreeNode(E e) {
-            element = e;
+        public Node(E data) {
+            element = data;
+            left = right = null;
         }
     }
 
-    private TreeNode<E> root;
-    private int size;
+    // BST root node
+    Node root;
 
-    public void insert(E e) {
-        // Implementation of the insert method
-        // ...
+    // Constructor for BST =>initial empty tree
+    BST() {
+        root = null;
     }
 
-    public void addAll(List<E> elements) {
-        for (E element : elements) {
-            insert(element);
+    // Insert a node in BST
+    void insert(E element) {
+        root = insertRecursive(root, element);
+    }
+
+    // Recursive insert function
+    Node insertRecursive(Node root, E element) {
+        // tree is empty
+        if (root == null) {
+            root = new Node(element);
+            return root;
+        }
+        // traverse the tree
+        int compareResult = element.compareTo(root.element);
+        if (compareResult < 0) // insert in the left subtree
+            root.left = insertRecursive(root.left, element);
+        else if (compareResult > 0) // insert in the right subtree
+            root.right = insertRecursive(root.right, element);
+        // return pointer
+        return root;
+    }
+
+    // Method for inorder traversal of BST
+    void inorder() {
+        inorderRecursive(root);
+    }
+
+    // Recursively traverse the BST
+    void inorderRecursive(Node root) {
+        if (root != null) {
+            inorderRecursive(root.left);
+            System.out.print(root.element + " ");
+            inorderRecursive(root.right);
         }
     }
 
-    @Override
-    public Iterator<E> iterator() {
-        return new BSTIterator();
+    // Returns an iterator for traversing the elements in a BST in preorder
+    public Iterator<E> preorderIterator() {
+        return new PreorderIterator(); // Create and return a new PreorderIterator
     }
 
-    private class BSTIterator implements Iterator<E> {
-        private TreeNode<E> current;
-        private java.util.Stack<TreeNode<E>> stack;
-    
-        public BSTIterator() {
-            stack = new java.util.Stack<>();
-            current = root;
-            initializeStack();
+    // Inner class PreorderIterator
+    private class PreorderIterator implements Iterator<E> {
+        // Store the elements in a list
+        private List<E> list = new ArrayList<>();
+        private int current = 0; // Point to the current element in list
+
+        public PreorderIterator() {
+            preorder(root); // Traverse binary tree and store elements in list
         }
-    
-        private void initializeStack() {
-            while (current != null) {
-                stack.push(current);
-                current = current.left;
+
+        // Preorder traversal from a subtree
+        private void preorder(Node root) {
+            if (root != null) {
+                list.add(root.element);
+                preorder(root.left);
+                preorder(root.right);
             }
         }
-    
+
+        // More elements for traversing?
         @Override
         public boolean hasNext() {
-            return !stack.isEmpty();
+            return current < list.size();
         }
-    
+
+        // Get the current element and move to the next
         @Override
         public E next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-    
-            TreeNode<E> temp = stack.pop();
-            current = temp.right;
-            initializeStack();
-    
-            return temp.element;
+            return list.get(current++);
+        }
+
+        // Optional: Implement the remove() method if needed
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
         }
     }
 
-    public static void main(String[] args) {
-        BST<Integer> bst = new BST<>();
-        bst.addAll(List.of(5, 2, 8, 1, 3, 7, 9));
+    // Nested class Main for testing
+    public static class Main {
+        public static void main(String[] args) {
+            // create a BST object
+            BST<Integer> bst = new BST<>();
+            // insert data into BST
+            bst.insert(45);
+            bst.insert(10);
+            bst.insert(7);
+            bst.insert(12);
+            bst.insert(90);
+            bst.insert(50);
 
-        // Use for-each loop
-        for (Integer value : bst) {
-            System.out.print(value + " ");
+            // print the BST inorder
+            System.out.println("\nThe BST Created with input data (Inorder):");
+            bst.inorder();
+
+            // Test the preorderIterator() method
+            System.out.println("\nThe BST elements using preorderIterator():");
+            Iterator<Integer> preorderIterator = bst.preorderIterator();
+            while (preorderIterator.hasNext()) {
+                System.out.println(preorderIterator.next() + " ");
+            }
         }
     }
 }
-
